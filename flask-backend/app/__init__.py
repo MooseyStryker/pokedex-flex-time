84c295a220fafe1ab970f98567d6647528e9d7d9
Stryker Huber <stryker.important@gmail.com>
2024-03-14 08:45:36 -0700

    ahh yess

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -1,6 +1,17 @@
 
 # import statement for CSRF
+from flask import Flask, render_template
+from flask_migrate import flask_migrate
+from app.config import Configuration
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+
+app = Flask(__name__)
+
+app.config.from_object(Configuration)
+
+# db.init_app(app)
+
+# Migrate(app, db)
 
 
 
